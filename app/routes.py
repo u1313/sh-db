bbf2c988c62860b13060b728d5bfc869d57f1947
u1313 <u1313@users.noreply.github.com>
2019-01-19 22:57:08 +0200

    templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    shelf = 'Shelf One'
+    boxes = [
+        {
+            'label': 'A1',
+            'content': 'stuff A'
+        },
+        {
+            'label': 'A2',
+            'content': 'stuff B'
+        }
+    ]
+    return render_template('index.html', title='Home', shelf=shelf, boxes=boxes)
