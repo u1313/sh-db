a55d60f2d5e67f00f88c1b9ca69c0586e6b2ba0b
u1313 <u1313@users.noreply.github.com>
2019-08-06 10:37:44 +0300

    models init

---
+++ app/models.py
@@ -0,0 +1,6 @@
+from app import db
+
+class Boxes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    description = db.Column(db.String(128))
