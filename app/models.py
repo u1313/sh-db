bc82774230d554d1b7c75397d0c57479095497b2
u1313 <u1313@users.noreply.github.com>
2019-08-24 20:20:31 +0300

    repr

--- app/models.py
+++ app/models.py
@@ -4,3 +4,6 @@ class Boxes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     description = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<Box {}>'.format(self.name)
