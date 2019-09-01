7951fde647e6670190f58c255d2bf1c24aabd662
u1313 <u1313@users.noreply.github.com>
2019-09-01 11:40:56 +0300

    images table

--- app/models.py
+++ app/models.py
@@ -3,15 +3,16 @@ from app import db
 class Images(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
-    img = db.Column(BLOB)
+    img = db.Column(db.BLOB)
     description = db.Column(db.String(128))
-    
+
     def __repr__(self): '<Img {}>'.format(self.name)
 
 class Boxes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     description = db.Column(db.String(128))
+    img_id = db.Column(db.Integer, db.ForeignKey('images.id'))
 
     def __repr__(self):
         return '<Box {}>'.format(self.name)
